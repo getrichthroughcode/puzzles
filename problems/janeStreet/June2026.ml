b80b727a8a94c0a5f4faa8c7261987a8b610392c
Abdoulaye Diallo <abdoulayediallo338@gmail.com>
2026-06-03 15:56:51 +0200

    [JS0626][FIRST TRY] Might be too retarded to solve this

---
+++ problems/janeStreet/June2026.ml
@@ -0,0 +1,49 @@
+(**
+   Regional Artwork 
+
+• Answers to different clues 
+
+1. frequent Frazier foe   - ALI 
+2. type of magazine you might start getting in your 20's - AARP (American Association of Retired Persons) ?  
+3. arguably, the story of our time (and a hint to answering all the other clues...) - AI
+4. stage name of a famous skeptic (with "The") - THE AMAZING RANDY
+5. Oscar-winning role from the 80's - AMADEUS
+6. often-inset pair, for short - AV (Audio visual)
+7. microwaveable snack brand that replaced a refrigerated product - ACT II
+8. where to find "take thou this vial" and "double, double toil and trouble" - ACT IV
+9. you might break it in Kabul - AFGHANI
+10.eponym of multiple continents (and the answer that should come last after sorting)
+11.onetime Outkast output - AQUEMINI  
+12.notable 2026 launch - ARTEMIS II
+13.the present, in the past - AGO 
+14.our nearest neighbors, in some sense (though we haven't visited yet...) - ALPHA CENTURI 
+
+The answers to these clues need sorting! Move them into their proper locations,
+then use those moves to populate the final clue, below.
+
+• Sorting 
+
+- Unsorted
+
+1. ALI 
+2. AARP
+3. AI
+4. THE AMAZING RANDY 
+5. AMADEUS
+6. AV
+7. ACT II 
+8. ACT IV 
+9. AFGHANI
+10. AMERIGO
+11. AQUEMINI 
+12. ARTEMIS II 
+13. AGO
+14. ALPHA CENTURI
+
+- sorted
+14. AMERIGO 
+
+Like WHAT THE FUCK 
+   *)
+
+
