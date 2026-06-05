f9cf729a238ad450d623021164b79d9647f63836
Abdoulaye Diallo <abdoulayediallo338@gmail.com>
2026-06-05 23:00:29 +0200

    [E003][FIX] Now the implemented function gives directly the solution without any post-processing nor a priori knowledge

--- problems/euler/e003.ml
+++ problems/euler/e003.ml
@@ -14,14 +14,22 @@ let is_prime (number : int) : bool =
     in 
     aux number 2
 
-let largest_prime_factor (number : int) (limit : int) : int list =
+let rec max (lst : int list) (acc : int) : int = 
+  match lst with
+  | [] -> acc
+  | h :: t -> if h > acc then max t h
+              else
+                max t acc
+
+
+let largest_prime_factor (number : int) : int =
   let rec aux (n : int) (acc : int) : int list =
-    if acc > limit then []
+    if float_of_int acc > sqrt (float_of_int number) then []
     else if is_prime acc && n mod acc = 0 then
       acc :: aux (n / acc) acc
     else
       aux n (acc + 1)
   in
-  aux number 2
+  max (aux number 2) 0
 
 
