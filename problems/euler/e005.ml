a0543cac60d6b54823a24624db7cb67d76ee46ec
Abdoulaye Diallo <abdoulayediallo338@gmail.com>
2026-06-29 18:08:46 +0200

    [E005][FIRST TRY] very easy, I have not seen any optimization I can do yet here, might refactor the least_common_multiple function one day though

--- problems/euler/e005.ml
+++ problems/euler/e005.ml
@@ -1,3 +1,15 @@
-#use "lib/math.ml"
+(*
+ 2520 is the smallest number that can be divided by each of the numbers from 1 to 10  without any remainder.
 
-let response = IntMath.least_common_multiple 20
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
+
+Pretty easy problem -- Just got to compute the least common multiple of the numbers 1 2 up to 20. 
+
+
+I have abstracted away the implementation of the solution on my personal ocaml math library in the 
+folder lib/math.ml
+ *)
+
+
+
+let response = Math.least_common_multiple 20
